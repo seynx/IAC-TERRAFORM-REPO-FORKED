b28a527453af9cddd80d9e6ee69f10c722428562
tberry147 <totimab@yahoo.com>
2022-05-16 23:18:07 -0400

    Adds output

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+
+#Outputs (To output various resources attributes)
+output "vpc_id" {
+  description = "The ID of the VPC"
+  sensitive = false
+  value = aws_vpc.example.id
+}
+
+output "aws_instance" {
+  description = "The Public IP of the instance"
+  sensitive = false
+  value = aws_instance.web.public_ip
+}
